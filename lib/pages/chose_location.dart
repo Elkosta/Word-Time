967d434507e84525eb551d810595379fe04f71ce
Elkosta <kostantinoprifti9@gmail.com>
2022-10-24 11:53:08 +0200

    setting the page

---
+++ lib/pages/chose_location.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class ChoseLocation extends StatefulWidget {
+  const ChoseLocation({Key? key}) : super(key: key);
+
+  @override
+  State<ChoseLocation> createState() => _ChoseLocationState();
+}
+
+class _ChoseLocationState extends State<ChoseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Choode location screen"),
+    );
+  }
+}
